8601bb2682be744c269011c1ae3e91daa5ce226d
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-05-07 16:27:17 -0300

    req7

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -35,6 +35,8 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
+    return [job for job in jobs if job['industry'] == industry]
+
     """Filters a list of jobs by industry
 
     Parameters
